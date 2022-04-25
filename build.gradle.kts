469590355563f586eb591f13cb935cff89654dca
ColaGom <bchoi000@gmail.com>
2022-04-25 14:12:55 +0900

    Fix publishing script

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.20"
+    `java-library`
     `maven-publish`
 }
 
-group = "com.linger.lift"
-version = "1.0.0-SNAPSHOT"
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
 
 repositories {
     mavenCentral()
@@ -15,6 +18,19 @@ repositories {
 dependencies {
     implementation("org.bitcoinj:bitcoinj-core:0.16.1")
     testImplementation(kotlin("test"))
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            val mavenJava by creating(MavenPublication::class) {
+                from(components["java"])
+                groupId = "com.github.linger-studio"
+                artifactId = "lift"
+                version = "1.0.0-rc02"
+            }
+        }
+    }
 }
 
 tasks.test {
